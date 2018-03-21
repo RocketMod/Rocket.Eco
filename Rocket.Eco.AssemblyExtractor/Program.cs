b2c7284190234f8ceec7c27e8bbceb66a7fe5634
ChubbyQuokka <graybad1@icloud.com>
2018-03-21 17:32:44 -0400

    Finished AssemblyExtractor

--- Rocket.Eco.AssemblyExtractor/Program.cs
+++ Rocket.Eco.AssemblyExtractor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -10,67 +11,83 @@ namespace Rocket.Eco.EcoAssemblyExtractor
     {
         static void Main(string[] args)
         {
-            Assembly assembly = Assembly.GetAssembly(typeof(Program));
+            string CurrentDir = Directory.GetCurrentDirectory();
+            string OutputDir = Path.Combine(CurrentDir, "Output/");
+            string EcoServer = Path.Combine(CurrentDir, "EcoServer.exe");
 
-            string directory = Directory.GetCurrentDirectory();
-
-            string[] files = Directory.GetFiles(directory)?.Where(x => x.EndsWith(".compressed", StringComparison.InvariantCultureIgnoreCase))?.ToArray() ?? default(string[]);
-
-            if (files.Length != 0)
+            if (!File.Exists(EcoServer))
             {
-                for (int i = 0; i < files.Length; i++)
+                Console.WriteLine("Please place this executable next to EcoServer.exe!");
+                EndProgram();
+            }
+
+            if (!Directory.Exists(OutputDir))
+            {
+                Directory.CreateDirectory(OutputDir);
+            }
+
+            Assembly eco = Assembly.LoadFile(EcoServer);
+            string[] resources = eco.GetManifestResourceNames().Where(x => x.EndsWith(".compressed", StringComparison.InvariantCultureIgnoreCase)).Where(x => x.StartsWith("costura.", StringComparison.InvariantCultureIgnoreCase)).ToArray();
+
+            for (int i = 0; i < resources.Length; i++)
+            {
+                string finalName = resources[i].Replace(".compressed", "").Replace("costura.", "");
+
+                try
                 {
-                    try
+                    byte[] finalFile;
+
+                    using (Stream stream = eco.GetManifestResourceStream(resources[i]))
                     {
-                        ExtractFile(files[i]);
-                        Console.WriteLine($"Successfully extracted {files[i]}!");
+                        finalFile = ExtractFile(stream);
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Failed to extract {files[i]}!");
-                        Console.WriteLine(e.Message);
-                        Console.WriteLine(e.StackTrace);
-                    }
+
+                    File.WriteAllBytes(Path.Combine(OutputDir, finalName), finalFile);
+                    Console.WriteLine($"Successfully extracted {finalName}!");
                 }
-
-                Console.WriteLine("All files have been extracted!");
+                catch (Exception e)
+                {
+                    Console.WriteLine($"An error occured while extracting {finalName}!");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
             }
-            else
-            {
-                Console.WriteLine("No compressed files were found!");
-            }
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            EndProgram();
         }
-
-        public static void ExtractFile(string file)
+        
+        static byte[] ExtractFile(Stream asmStream)
         {
             byte[] bytes;
 
-            using (FileStream fileStream = File.Open(file, FileMode.Open))
+            using (DeflateStream deflateStream = new DeflateStream(asmStream, CompressionMode.Decompress))
             {
-                using (DeflateStream deflateStream = new DeflateStream(fileStream, CompressionMode.Decompress))
+                using (MemoryStream memStream = new MemoryStream())
                 {
-                    using (MemoryStream memStream = new MemoryStream())
+                    byte[] array = new byte[81920];
+                    int count;
+
+                    while ((count = deflateStream.Read(array, 0, array.Length)) != 0)
                     {
-                        byte[] array = new byte[81920];
-                        int count;
-
-                        while ((count = deflateStream.Read(array, 0, array.Length)) != 0)
-                        {
-                            memStream.Write(array, 0, count);
-                        }
-
-                        memStream.Position = 0;
-
-                        bytes = new byte[memStream.Length];
-                        memStream.Read(bytes, 0, bytes.Length);
+                        memStream.Write(array, 0, count);
                     }
+
+                    memStream.Position = 0;
+
+                    bytes = new byte[memStream.Length];
+                    memStream.Read(bytes, 0, bytes.Length);
                 }
             }
 
-            File.WriteAllBytes(file.Replace(".compressed", string.Empty).Replace("costura.", string.Empty), bytes);
+            return bytes;
+        }
+
+        static void EndProgram()
+        {
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            Process.GetCurrentProcess().Kill();
         }
     }
 }
