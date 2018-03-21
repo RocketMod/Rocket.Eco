8fa2d4f2faa3839e601cab6b48f95a910dcfd69f
ChubbyQuokka <graybad1@icloud.com>
2018-03-21 11:25:35 -0400

    Added RocketLauncher

---
+++ Rocket.Eco.Launcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Rocket.Eco.Launcher
+{
+    class MainClass
+    {
+        static Assembly asm;
+
+        public static void Main(string[] args)
+        {
+            string file = Path.Combine(Directory.GetCurrentDirectory(), "EcoServer.exe");
+            asm = Assembly.LoadFile(file);
+
+            MethodInfo attachAssemblies = asm.GetType("Costura.AssemblyLoader").GetMethod("Attach", BindingFlags.Static | BindingFlags.Public);
+            MethodInfo startServer = asm.GetType("Eco.Server.Startup").GetMethod("Start", BindingFlags.Static | BindingFlags.Public);
+
+            attachAssemblies.Invoke(null, null);
+            RocketAttacher.InitialzeRocketHook();
+            startServer.Invoke(null, new object[] { args });
+        }
+    }
+}
---
+++ Rocket.Eco.Launcher/RocketAttacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rocket.Eco.Launcher
+{
+    static class RocketAttacher
+    {
+        public static void InitialzeRocketHook()
+        {
+            //I guess I need to do something here...
+        }
+    }
+}
