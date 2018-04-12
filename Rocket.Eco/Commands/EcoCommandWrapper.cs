4c3a16ffea8343080c514234cdfbb3684abecfa4
ChubbyQuokka <graybad1@icloud.com>
2018-04-11 22:43:20 -0400

    Implemented the EcoCommandWrapper

--- Rocket.Eco/Commands/EcoCommandWrapper.cs
+++ Rocket.Eco/Commands/EcoCommandWrapper.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 using Eco.Gameplay.Systems.Chat;
 
 using Rocket.API.Commands;
+using Rocket.API.Logging;
+using Rocket.Eco.Player;
 
 namespace Rocket.Eco.Commands
 {
@@ -13,14 +16,54 @@ namespace Rocket.Eco.Commands
         public string Permission => $"Eco.Base.{Name}";
 
         readonly ChatCommandAttribute command;
+        readonly MethodInfo commandMethod;
 
-        internal EcoCommandWrapper(ChatCommandAttribute command)
+        static ChatManager ecoChatManager;
+        static MethodInfo execute;
+
+        internal EcoCommandWrapper(MethodInfo method)
         {
-            this.command = command;
+            if (ecoChatManager == null)
+            {
+                ecoChatManager = (ChatManager)typeof(ChatServer).GetField("netChatManager", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(ChatServer.Obj);
+            }
+
+            if (execute == null)
+            {
+                execute = typeof(ChatManager).GetMethod("InvokeCommand", BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+
+            command = (ChatCommandAttribute)method.GetCustomAttributes().FirstOrDefault(x => x is ChatCommandAttribute);
+
+            if (command != null)
+            {
+                commandMethod = method;
+            }
+            else
+            {
+                Eco.runtime.Container.Get<ILogger>().LogError("An attempt was made to register a vanilla command with inproper attributes!");
+            }
         }
 
         public void Execute(ICommandContext context)
         {
+            if (context.Caller is EcoPlayer p)
+            {
+                string args = string.Join(",", context.Parameters);
+
+                try
+                {
+                    execute.Invoke(ecoChatManager, new object[] { Name, commandMethod, args, p.User });
+                }
+                catch
+                {
+                    Eco.runtime.Container.Get<ILogger>().LogError($"{p.Name} failed to execute the command `{Name}`!");
+                }
+            }
+            else
+            {
+                context.Caller.SendMessage("Only an in-game user may call vanilla Eco commands!");
+            }
         }
     }
 }
