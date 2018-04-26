fd5e1b29fc916e3947ae29e64c0b42fa4302e359
ChubbyQuokka <graybad1@icloud.com>
2018-04-25 21:16:19 -0400

    ..

--- Rocket.Eco/Legislation/EcoGovernment.cs
+++ Rocket.Eco/Legislation/EcoGovernment.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using Rocket.API.DependencyInjection;
 using Rocket.Eco.API;
 using Rocket.Eco.API.Legislation;
@@ -10,7 +10,6 @@ namespace Rocket.Eco.Legislation
 {
     public sealed class EcoGovernment : ContainerAccessor, IGovernment
     {
-        private readonly IDependencyContainer governmentContainer;
         private readonly EcoLegislation ecoLegislation;
 
         public IElection CurrentElection { get; private set; }
@@ -19,27 +18,12 @@ namespace Rocket.Eco.Legislation
         public EcoGovernment(EcoLegislation ecoLegislation, IDependencyContainer container) : base(container)
         {
             this.ecoLegislation = ecoLegislation;
-            governmentContainer = container.CreateChildContainer();
 
             //TODO: Pull all info from EcoLegislation.
         }
 
-        public IEnumerable<ILaw> Laws
-        {
-            get
-            {
-                governmentContainer.TryGetAll<ILaw>(out IEnumerable<ILaw> laws);
-                return laws;
-            }
-        }
+        public IEnumerable<ILaw> Laws => throw new NotImplementedException();
 
-        public IEnumerable<IElection> PastElections
-        {
-            get
-            {
-                governmentContainer.TryGetAll<IElection>(out IEnumerable<IElection> elections);
-                return elections.Where(x => x.IsFinished);
-            }
-        }
+        public IEnumerable<IElection> PastElections => throw new NotImplementedException();
     }
 }
