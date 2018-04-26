4c93a4e7163be1589ed42ed158e3c276d21089db
ChubbyQuokka <graybad1@icloud.com>
2018-04-26 15:12:41 -0400

    Changed BaseTask to ThreadSafeTask

---
+++ Rocket.Eco/Scheduling/ThreadSafeTask.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Rocket.API;
+using Rocket.API.Scheduler;
+
+namespace Rocket.Eco.Scheduling
+{
+    public sealed class ThreadSafeTask : ITask
+    {
+        private readonly ITaskScheduler scheduler;
+
+        public ThreadSafeTask(ITaskScheduler scheduler, ILifecycleObject owner, Action action, ExecutionTargetContext executionTargetContext)
+        {
+            this.scheduler = scheduler;
+            Owner = owner;
+            Action = action;
+            ExecutionTarget = executionTargetContext;
+        }
+
+        private Exception exception;
+        private readonly object exceptionLock = new object();
+
+        public Exception Exception
+        {
+            get
+            {
+                lock (exceptionLock)
+                {
+                    return exception;
+                }
+            }
+            internal set
+            {
+                lock (exceptionLock)
+                {
+                    exception = value;
+                }
+            }
+        }
+
+        public ILifecycleObject Owner { get; }
+        public Action Action { get; }
+        public ExecutionTargetContext ExecutionTarget { get; }
+        
+        private bool isCancelled;
+        private readonly object isCancelledLock = new object();
+
+        private bool isFinished;
+        private readonly object isFinishedLock = new object();
+
+        public bool IsCancelled
+        {
+            get
+            {
+                lock (isCancelledLock)
+                {
+                    return isCancelled;
+                }
+            }
+            internal set
+            {
+                lock (isCancelledLock)
+                {
+                    isCancelled = value;
+                }
+            }
+        }
+
+        public bool IsFinished
+        {
+            get
+            {
+                lock (isFinishedLock)
+                {
+                    return isFinished;
+                }
+            }
+            internal set
+            {
+                lock (isFinishedLock)
+                {
+                    isFinished = value;
+                }
+            }
+        }
+
+        public void Cancel()
+        {
+            scheduler.CancelTask(this);
+        }
+    }
+}
