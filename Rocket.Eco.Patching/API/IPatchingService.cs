17476eec3d4d570afea6b4786bcd465b1a066bed
ChubbyQuokka <graybad1@icloud.com>
2018-06-10 20:25:27 -0400

    Finished patching service, time to implement

--- Rocket.Eco.Patching/API/IPatchingService.cs
+++ Rocket.Eco.Patching/API/IPatchingService.cs
@@ -20,20 +20,19 @@ namespace Rocket.Eco.Patching.API
         /// <summary>
         ///     Registers an assembly given the <see cref="AssemblyDefinition" />.
         /// </summary>
-        /// <param name="assemblyDefinition">The assembly to register.</param>
         /// <returns>
         ///     <value>false</value>
         ///     when the requested <see cref="AssemblyDefinition" /> has no awaiting <see cref="IAssemblyPatch" />, else
         ///     <value>true</value>
         ///     .
         /// </returns>
-        /// <param name="resolver">The <see cref="IAssemblyResolver" /> to register.</param>
-        bool RegisterAssembly(AssemblyDefinition assemblyDefinition, IAssemblyResolver resolver = null);
+        /// <param name="assemblyDefinition">The <see cref="AssemblyDefinition" /> to register.</param>
+        bool RegisterAssembly(AssemblyDefinition assemblyDefinition);
 
         /// <summary>
         ///     Patches every given Assembly up-to this point and wipes the list of assemblies/patches.
         /// </summary>
         /// <returns>An enumaration of the patched <see cref="AssemblyDefinition" />.</returns>
-        IEnumerable<AssemblyDefinition> FinalizePass();
+        IEnumerable<AssemblyDefinition> Patch();
     }
 }
---
+++ Rocket.Eco.Patching/PatchService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mono.Cecil;
+using Rocket.Eco.Patching.API;
+
+namespace Rocket.Eco.Patching
+{
+    /// <inheritdoc />
+    public sealed class PatchService : IPatchingService
+    {
+        private readonly List<AssemblyDefinition> assemblies = new List<AssemblyDefinition>();
+        private readonly List<IAssemblyPatch> patches = new List<IAssemblyPatch>();
+
+        private readonly object lockObj = new object();
+
+        /// <inheritdoc />
+        public bool RegisterPatch<T>() where T : IAssemblyPatch, new()
+        {
+            lock (lockObj)
+            {
+                if (patches.FirstOrDefault(x => x.GetType() == typeof(T)) != null)
+                    return false;
+
+                patches.Add(new T());
+                return true;
+            }
+        }
+
+        /// <inheritdoc />
+        public bool RegisterAssembly(AssemblyDefinition assemblyDefinition)
+        {
+            lock (lockObj)
+            {
+                if (assemblies.FirstOrDefault(x => x.Name.Name.Equals(assemblyDefinition.Name.Name, StringComparison.InvariantCultureIgnoreCase)) != null)
+                    return false;
+
+                assemblies.Add(assemblyDefinition);
+                return true;
+            }
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<AssemblyDefinition> Patch()
+        {
+            lock (lockObj)
+            {
+                foreach (AssemblyDefinition asmDef in assemblies)
+                {
+                    foreach (IAssemblyPatch patch in patches)
+                    {
+                        foreach (ModuleDefinition modDef in asmDef.Modules)
+                        {
+                            TypeDefinition typeDef = modDef.Types.FirstOrDefault(x => x.FullName.Equals(patch.TargetType, StringComparison.InvariantCultureIgnoreCase));
+
+                            if (typeDef == null)
+                                continue;
+
+                            patch.Patch(typeDef);
+
+                            break;
+                        }
+                    }
+                }
+
+                AssemblyDefinition[] asmArray = assemblies.ToArray();
+
+                assemblies.Clear();
+                patches.Clear();
+
+                return asmArray;
+            }
+        }
+    }
+}
