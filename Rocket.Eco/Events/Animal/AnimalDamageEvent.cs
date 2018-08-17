977d4810b7bcf7f5f5ad401cc0e6c5b1a09b2637
ChubbyQuokka <graybad1@icloud.com>
2018-08-16 21:31:59 -0400

    Began implementing EcoAnimal and the associated events

---
+++ Rocket.Eco/Events/Animal/AnimalDamageEvent.cs
@@ -0,0 +1,21 @@
+﻿#if DEBUG
+using Rocket.API.Eventing;
+using Rocket.Eco.Entity;
+
+namespace Rocket.Eco.Events.Animal
+{
+    public sealed class AnimalDamageEvent : AnimalEvent, ICancellableEvent
+    {
+        public bool IsCancelled { get; set; }
+
+        public float Damage { get; set; }
+
+        public float CurrentHealth => (float)Animal.Health;
+
+        public AnimalDamageEvent(EcoAnimal animal, float damage) : base(animal)
+        {
+            Damage = damage;
+        }
+    }
+}
+#endif
---
+++ Rocket.Eco/Events/Animal/AnimalSpawnEvent.cs
@@ -0,0 +1,14 @@
+﻿#if DEBUG
+using Rocket.API.Eventing;
+using Rocket.Eco.Entity;
+
+namespace Rocket.Eco.Events.Animal
+{
+    public sealed class AnimalSpawnEvent : AnimalEvent, ICancellableEvent
+    {
+        public AnimalSpawnEvent(EcoAnimal animal) : base(animal) { }
+
+        public bool IsCancelled { get; set; }
+    }
+}
+#endif
