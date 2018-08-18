fddf11a0011fc2f68f26a19a4f4257786f9f25d7
ChubbyQuokka <graybad1@icloud.com>
2018-08-18 11:42:28 -0400

    Removed useless locks and fixed output assembly

--- Rocket.Patching/PatchingService.cs
+++ Rocket.Patching/PatchingService.cs
@@ -11,70 +11,60 @@ namespace Rocket.Patching
     {
         private readonly List<AssemblyDefinition> assemblies = new List<AssemblyDefinition>();
 
-        private readonly object lockObj = new object();
         private readonly List<IAssemblyPatch> patches = new List<IAssemblyPatch>();
 
         /// <inheritdoc />
         public bool RegisterPatch<T>() where T : IAssemblyPatch, new()
         {
-            lock (lockObj)
-            {
-                if (patches.FirstOrDefault(x => x.GetType() == typeof(T)) != null)
-                    return false;
+            if (patches.FirstOrDefault(x => x.GetType() == typeof(T)) != null)
+                return false;
 
-                patches.Add(new T());
-                return true;
-            }
+            patches.Add(new T());
+            return true;
         }
 
         /// <inheritdoc />
         public bool RegisterAssembly(AssemblyDefinition assemblyDefinition)
         {
-            lock (lockObj)
-            {
-                if (assemblies.FirstOrDefault(x => x.Name.Name.Equals(assemblyDefinition.Name.Name, StringComparison.InvariantCultureIgnoreCase)) != null)
-                    return false;
+            if (assemblies.FirstOrDefault(x => x.Name.Name.Equals(assemblyDefinition.Name.Name, StringComparison.InvariantCultureIgnoreCase)) != null)
+                return false;
 
-                assemblies.Add(assemblyDefinition);
-                return true;
-            }
+            assemblies.Add(assemblyDefinition);
+            return true;
         }
 
         /// <inheritdoc />
         public IEnumerable<AssemblyDefinition> Patch()
         {
-            lock (lockObj)
+            foreach (AssemblyDefinition asmDef in assemblies)
             {
-                foreach (AssemblyDefinition asmDef in assemblies)
+                foreach (IAssemblyPatch patch in patches)
                 {
-                    foreach (IAssemblyPatch patch in patches)
+                    foreach (ModuleDefinition modDef in asmDef.Modules)
                     {
-                        foreach (ModuleDefinition modDef in asmDef.Modules)
-                        {
-                            TypeDefinition typeDef = modDef.Types.FirstOrDefault(x => x.FullName.Equals(patch.TargetType, StringComparison.InvariantCultureIgnoreCase));
+                        TypeDefinition typeDef = modDef.Types.FirstOrDefault(x => x.FullName.Equals(patch.TargetType, StringComparison.InvariantCultureIgnoreCase));
 
-                            if (typeDef == null)
-                                continue;
+                        if (typeDef == null)
+                            continue;
 
-                            patch.Patch(typeDef);
+                        patch.Patch(typeDef);
 
-                            Console.WriteLine($"The patch `{patch.GetType().FullName}` has been applied to `{typeDef.FullName}`");
+                        Console.WriteLine($"The patch `{patch.GetType().FullName}` has been applied to `{typeDef.FullName}`");
 
-                            break;
-                        }
+                        break;
                     }
                 }
-
-                //I hope this kills the Stream...
-                assemblies.ForEach(x => x.Dispose());
-
-                AssemblyDefinition[] asmArray = assemblies.ToArray();
-
-                assemblies.Clear();
-                patches.Clear();
-
-                return asmArray;
             }
+
+            //I hope this kills the Stream...
+            assemblies.ForEach(x => x.Dispose());
+
+            AssemblyDefinition[] asmArray = assemblies.ToArray();
+
+            assemblies.Clear();
+            patches.Clear();
+
+            return asmArray;
         }
     }
 }
